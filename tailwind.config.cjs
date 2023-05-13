638b6bc2eb409dca0596e78bade423368fcea3d4
IreneVivoBook14 <irena04florencia13@gmail.com>
2023-05-14 01:40:50 +0700

    input semua warna

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,12 @@ const config = {
   theme: {
     extend: {
       colors: {
-        'ultramarine' : '#3566FF'
+        'ultramarine' : '#3566FF',
+        'white' : '#FFFFFF',
+        'dark-blue' : '#232B53',
+        'black' : '#000000',
+        'ultramarine-max' : '#4249FF',
+        'ultramarine-min' : '#00D1FF'
       }
     },
   },
