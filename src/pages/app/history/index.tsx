65431169be9b0970650136a68d83aa7dc6c78692
MangoMbp <notwatermango@gmail.com>
2023-05-15 17:04:17 +0700

    add backend ticket list

--- src/pages/app/history/index.tsx
+++ src/pages/app/history/index.tsx
@@ -1,8 +1,18 @@
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 import AppLayout from "~/components/app/AppLayout";
 import type { AuthNextPage } from "~/types/pages";
+import { api } from "~/utils/api";
 
 const HistoryPage: AuthNextPage = () => {
+  // get history by list
+  const { data: sessionData } = useSession();
+  const { data: account } = api.account.getUserId.useQuery({
+    accountId: sessionData ? sessionData.user.id : "",
+  });
+  const { data: user } = api.user.getUserWithTicketList.useQuery({
+    userId: account?.user ? account.user.id : "",
+  });
   return (
     <>
       <Head>
@@ -13,6 +23,9 @@ const HistoryPage: AuthNextPage = () => {
       <AppLayout>
         <h1>History</h1>
         <div>list nya</div>
+        {user?.tickets.map((ticket) => {
+          return <div key={ticket.id}>{ticket.merchant?.name}</div>;
+        })}
       </AppLayout>
     </>
   );
--- src/server/api/routers/ticket.ts
+++ src/server/api/routers/ticket.ts
@@ -1,4 +1,3 @@
-import type { Merchant } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
@@ -44,40 +43,5 @@ export const ticketRouter = createTRPCRouter({
           message: `Error creating ticket'`,
         });
       }
-    }),
-  getMerchantInfoOnTicketConfirmation: protectedProcedure
-    .input(
-      z.object({
-        merchantId: z.string(),
-      })
-    )
-    .query(async ({ input, ctx }) => {
-      if (input.merchantId == "test") {
-        const dummy = {} as Merchant;
-        dummy.imageUrl = "https://i.ibb.co/BTGq4c4/borgir.png";
-        dummy.name = "Borger Qhinks";
-        dummy.id = "test";
-        dummy.isOpen = true;
-        return dummy;
-      }
-
-      const merchant = await ctx.prisma.merchant.findUnique({
-        where: {
-          id: input.merchantId,
-        },
-        select: {
-          id: true,
-          name: true,
-          isOpen: true,
-          imageUrl: true,
-        },
-      });
-      if (!merchant) {
-        throw new TRPCError({
-          code: "NOT_FOUND",
-          message: `No merchant found'`,
-        });
-      }
-      return merchant;
-    }),
+    })
 });
