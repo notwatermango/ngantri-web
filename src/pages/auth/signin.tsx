b959f40149f8ba3ee5e1ce79dc58eb9e1f785abb
IreneVivoBook14 <irena04florencia13@gmail.com>
2023-05-16 12:19:37 +0700

    sign in page

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -9,16 +9,47 @@ export default function SignIn({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <form method="post" action="/api/auth/callback/credentials">
-      <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-      <label>
-        Email
-        <input name="email" type="text" />
-      </label>
-      <label>
-        Password
-        <input name="password" type="password" />
-      </label>
-      <button type="submit">Sign in</button>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-ultramarine  gap-y-5 p-5">
+        
+        <img
+          className="h-10 w-auto rounded"
+          src="https://i.ibb.co/rHScgKG/logo-pjg-lgt.png"
+          alt="logo pjg light"
+        />
+        <p className="w-48 text-center font-semibold text-white ">
+          Reserve your seat wherever and whenever
+        </p>
+        <img
+          className="h-24 w-auto rounded"
+          src="https://i.ibb.co/rknCvm4/logo-kcl-lgt.png"
+          alt="logo pjg light"
+        />
+          
+        <div className="flex flex-col items-center gap-y-3 p-10 ">
+          <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+          <label className="font-semibold text-white">
+            EMAIL :
+            <input className="bg-white w-80 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+              placeholder="example@gmail.com"
+              name="email" 
+              type="email" 
+              required/>
+          </label>
+          <label className="font-semibold text-white">
+            PASSWORD :
+            <input 
+              className="bg-white w-80 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              name="password" 
+              type="password" 
+              required/>
+          </label>
+        </div>
+    
+        <button className="h-10 w-80 rounded-full bg-dark-blue text-white font-bold  no-underline transition hover:bg-ultramarine-min hover:text-white"
+          type="submit">
+            SIGN IN
+        </button>
+      </main>
     </form>
   );
 }
