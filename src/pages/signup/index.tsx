553f47db9c27f1e7cb045a0675c790bbcca48266
MangoMbp <notwatermango@gmail.com>
2023-05-16 18:17:13 +0700

    ran prettier

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -12,19 +12,19 @@ const SignUpPage: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-ultramarine">
         <div className="flex flex-col items-center gap-y-5 p-5">
-            <img
-              className="h-10 w-auto rounded"
-              src="https://i.postimg.cc/jLkDY68S/logo-pjg-lgt.png"
-              alt="logo pjg light"
-            />
-            <p className="w-48 text-center font-semibold text-white ">
-              Reserve your seat wherever and whenever
-            </p>
-            <img
-              className="h-24 w-auto rounded"
-              src="https://i.postimg.cc/JsDGrygc/logo-kcl-lgt.png"
-              alt="logo kcl light"
-            />
+          <img
+            className="h-10 w-auto rounded"
+            src="https://i.postimg.cc/jLkDY68S/logo-pjg-lgt.png"
+            alt="logo pjg light"
+          />
+          <p className="w-48 text-center font-semibold text-white ">
+            Reserve your seat wherever and whenever
+          </p>
+          <img
+            className="h-24 w-auto rounded"
+            src="https://i.postimg.cc/JsDGrygc/logo-kcl-lgt.png"
+            alt="logo kcl light"
+          />
         </div>
         <div className="flex flex-col items-center gap-y-3 p-10">
           <Link href="/signup/user">
@@ -33,7 +33,7 @@ const SignUpPage: NextPage = () => {
             </button>
           </Link>
           <Link href="/signup/merchant">
-            <button className="h-10 w-80 rounded-full text-dark-blue font-bold bg-white no-underline transition hover:bg-ultramarine-min hover:text-white ">
+            <button className="h-10 w-80 rounded-full bg-white font-bold text-dark-blue no-underline transition hover:bg-ultramarine-min hover:text-white ">
               AS MERCHANT
             </button>
           </Link>
