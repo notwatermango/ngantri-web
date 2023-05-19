d7f03e6c3c677890a2bc71240caa8fb59d70e192
ttarreuu <tarahanifanfaza@gmail.com>
2023-05-19 15:41:35 +0700

    styling dashboard merchant page

--- src/pages/merchant/dashboard.tsx
+++ src/pages/merchant/dashboard.tsx
@@ -11,7 +11,44 @@ const MerchantDashboard: AuthNextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MerchantLayout>
-        <h1>Dashboard bro</h1>
+        <div className="flex h-full w-full flex-col justify-center bg-ultramarine px-5 md:p-8">
+          <div className="flex flex-col items-center p-5">
+            <img
+              className="h-10 w-auto rounded"
+              src="https://i.ibb.co/hKQPjS6/Group-35-1.png"
+              alt="long-logo"
+            />
+          </div>
+          <div className="mt-5 flex flex-col items-center">
+            <h5 className="text-lg font-bold text-white">WELCOME,</h5>
+            <h6 className="text-md text-white">New Store - Paskal 23</h6>
+          </div>
+          <div className="mt-5 flex justify-between">
+            <div className="flex flex-col justify-between">
+              <h5 className="text-lg font-bold text-white">WELCOME,</h5>
+              <h6 className="text-md text-white">New Store - Paskal 23</h6>
+            </div>
+            <div>
+              <button className="text-md block h-full rounded-lg bg-green-500 p-1 font-bold text-white">
+                OPEN
+              </button>
+            </div>
+          </div>
+          <div className="mb-5 mt-10 flex w-full resize-none items-center justify-between rounded-lg border border-white bg-white">
+            <div className="m-5 text-lg font-bold text-black">IN QUEUE</div>
+            <div className="mx-5 text-4xl font-bold text-black">14</div>
+          </div>
+          <div className="flex justify-between">
+            <div className="w-5/12 resize-none flex-col items-center justify-between rounded-lg border border-white bg-white p-5">
+              <div className="text-lg font-bold text-black">FINISH</div>
+              <div className="mt-5 text-4xl font-bold text-black">14</div>
+            </div>
+            <div className="w-5/12 resize-none flex-col items-center justify-between rounded-lg border border-white bg-white p-5">
+              <div className="text-lg font-bold text-black">CANCELED</div>
+              <div className="mt-5 text-4xl font-bold text-black">14</div>
+            </div>
+          </div>
+        </div>
       </MerchantLayout>
     </>
   );
