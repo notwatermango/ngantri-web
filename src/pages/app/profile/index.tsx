b325cd37f9da00b9d9df6a7ebd14fb347c7c091e
MangoMbp <notwatermango@gmail.com>
2023-05-21 22:12:48 +0700

    fix user profile layout

--- src/pages/app/profile/index.tsx
+++ src/pages/app/profile/index.tsx
@@ -19,36 +19,49 @@ const ProfilePage: AuthNextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <AppLayout>
-        <div className="flex h-full w-full flex-col justify-center bg-ultramarine px-10 py-16 md:p-8">
-          <div>
-            <div className="flex flex-col items-center p-5">
-              <img
-                className="h-10 w-auto rounded"
-                src="https://i.ibb.co/hKQPjS6/Group-35-1.png"
-                alt="long-logo"
-              />
-            </div>
-            <h1 className="my-5 text-center text-2xl font-bold leading-none text-white dark:text-white md:text-5xl lg:text-6xl ">
+        <div className="flex h-full w-full flex-col justify-center gap-y-5 overflow-y-auto bg-ultramarine p-5 md:p-8">
+          <div className="flex flex-col items-center gap-y-3">
+            <img
+              className="h-10 w-auto rounded"
+              src="https://i.ibb.co/hKQPjS6/Group-35-1.png"
+              alt="long-logo"
+            />
+            <h1 className="text-center text-2xl font-bold leading-none text-white dark:text-white md:text-5xl lg:text-6xl ">
               PROFILE
             </h1>
-            <div className="mt-2 text-lg font-bold text-white">NAME</div>
-            <div className="items-strecth text-md row-span-2 my-1 flex h-auto w-full flex-row justify-between rounded-lg border border-white bg-white p-1 text-start text-black">
-              <div className="w-4/6 pl-1 pt-0.5">{user?.name}</div>
-              <button className="block w-2/6 rounded-xl bg-ultramarine text-lg font-bold">
-                Edit
-              </button>
+          </div>
+          <div className="flex w-full flex-col gap-y-3">
+            <div>
+              <div className="text-lg font-bold text-white">NAME</div>
+              <div className="items-strecth text-md row-span-2 my-1 flex h-auto w-full flex-row justify-between rounded-lg border border-white bg-white p-1 text-start text-black">
+                <div className="w-4/6 pl-1 pt-0.5">{user?.name}</div>
+                <button className="block w-2/6 rounded-xl bg-ultramarine text-lg font-bold">
+                  Edit
+                </button>
+              </div>
             </div>
-            <div className="mt-5 text-lg font-bold text-white">EMAIL</div>
-            <div className="items-strecth text-md row-span-2 my-1 flex h-auto w-full flex-row justify-between rounded-lg border border-white bg-white p-1 text-start text-black">
-              <div className="w-4/6 pl-1 pt-0.5">{user?.email}</div>
+            <div>
+              <div className="text-lg font-bold text-white">EMAIL</div>
+              <div className="items-strecth text-md row-span-2 my-1 flex h-auto w-full flex-row justify-between rounded-lg border border-white bg-white p-1 text-start text-black">
+                <div className="w-4/6 pl-1 pt-0.5">{user?.email}</div>
+              </div>
             </div>
-            <div className="mt-5 text-lg font-bold text-white">PHONE</div>
-            <div className="items-strecth text-md row-span-2 my-1 flex h-auto w-full flex-row justify-between rounded-lg border border-white bg-white p-1 text-start text-black">
-              <div className="w-4/6 pl-1 pt-0.5">{user?.phone}</div>
+            <div>
+              <div className="text-lg font-bold text-white">PHONE</div>
+              <div className="items-strecth text-md row-span-2 my-1 flex h-auto w-full flex-row justify-between rounded-lg border border-white bg-white p-1 text-start text-black">
+                <div className="w-4/6 pl-1 pt-0.5">
+                  {user ? user.phone : "-"}
+                </div>
+              </div>
             </div>
-            <div className="mt-5 text-lg font-bold text-white">ADDRESS</div>
-            <div className="items-strecth text-md row-span-2 my-1 flex h-auto w-full flex-row justify-between rounded-lg border border-white bg-white p-1 text-start text-black">
-              <div className="w-4/6 pl-1 pt-0.5">{user?.address}</div>
+            <div>
+              <div className="text-lg font-bold text-white">ADDRESS</div>
+              <div className="items-strecth text-md row-span-2 my-1 flex h-auto w-full flex-row justify-between rounded-lg border border-white bg-white p-1 text-start text-black">
+                <div className="w-4/6 pl-1 pt-0.5">
+                  {/* Case optional: set placeholder "-" */}
+                  {user ? user.address : "-"}
+                </div>
+              </div>
             </div>
           </div>
         </div>
