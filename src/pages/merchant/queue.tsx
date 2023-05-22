d402747a734f2897b3223cedf9cd6bab125fbc45
MangoMbp <notwatermango@gmail.com>
2023-05-22 16:54:54 +0700

    implement barebone call customer and finish ticket

--- src/pages/merchant/queue.tsx
+++ src/pages/merchant/queue.tsx
@@ -7,8 +7,22 @@ const MerchantQueue: AuthNextPage = () => {
   const { data: merchant, refetch } =
     api.merchant.getMerchantActiveQueueList.useQuery();
   const cancelTicket = api.ticket.cancelTicket.useMutation();
+  const finishTicket = api.ticket.finishTicket.useMutation();
+  const callCustomer = api.ticket.callTicketCustomer.useMutation();
   const handleCancelTicket = async (ticketId: string) => {
     await cancelTicket.mutateAsync({
+      ticketId,
+    });
+    await refetch();
+  };
+  const handleFinishTicket = async (ticketId: string) => {
+    await finishTicket.mutateAsync({
+      ticketId,
+    });
+    await refetch();
+  };
+  const handleCallCustomer = async (ticketId: string) => {
+    await callCustomer.mutateAsync({
       ticketId,
     });
     await refetch();
@@ -64,7 +78,10 @@ const MerchantQueue: AuthNextPage = () => {
                             />
                           </svg>
                         </div>
-                        <div className="group flex h-full items-center rounded-lg bg-indigo-100 fill-amber-500 px-2 hover:cursor-pointer">
+                        <div
+                          onClick={() => handleCallCustomer(ticket.id)}
+                          className="group flex h-full items-center rounded-lg bg-indigo-100 fill-amber-500 px-2 hover:cursor-pointer"
+                        >
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="24"
@@ -87,7 +104,10 @@ const MerchantQueue: AuthNextPage = () => {
                             <path d="M15.137 3.945c-.644-.374-1.042-1.07-1.04-1.82v-.003c0-1.172-.939-2.122-2.097-2.122s-2.097.95-2.097 2.122v.003c.002.751-.396 1.446-1.04 1.82-4.668 2.712-1.986 11.715-6.863 13.306v1.749h20v-1.749c-4.877-1.591-2.195-10.594-6.863-13.306zm-3.137-2.945c.552 0 1 .449 1 1 0 .552-.448 1-1 1s-1-.448-1-1c0-.551.448-1 1-1zm3 20c0 1.598-1.392 3-2.971 3s-3.029-1.402-3.029-3h6zm5.015-12.521c-.246-1.504-.933-3.682-2.817-5.515l1.396-1.434c1.8 1.752 2.974 4.044 3.395 6.626l-1.974.323zm-18.015-.322c.421-2.583 1.595-4.874 3.395-6.627l1.396 1.434c-1.884 1.833-2.572 4.011-2.817 5.515l-1.974-.322z" />
                           </svg>
                         </div>
-                        <div className="group flex h-full items-center rounded-lg bg-indigo-100 fill-emerald-500 px-2 hover:cursor-pointer">
+                        <div
+                          onClick={() => handleFinishTicket(ticket.id)}
+                          className="group flex h-full items-center rounded-lg bg-indigo-100 fill-emerald-500 px-2 hover:cursor-pointer"
+                        >
                           <svg
                             stroke-linejoin="round"
                             width="24"
